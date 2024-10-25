024e65d4cc8109414492e6b982116bf374531604
Robin <robin.tordly@nav.no>
2024-10-25 13:12:30 +0200

    log

--- lua/neotest-klib/init.lua
+++ lua/neotest-klib/init.lua
@@ -1,36 +1,50 @@
 local lib = require("neotest.lib")
-local async = require("neotest.async")
 local output = require("neotest-klib.output")
 local spec = require("neotest-klib.spec")
 local pos = require("neotest-klib.pos")
+local log = require("neotest.logging")
 
 local adapter = { name = "neotest-klib" }
 
-
 ---@async
+---@param dir string @Directory to treat as cwd
+---@return string | nil @Absolute root dir of test suite
 function adapter.root(dir)
-  return lib.files.match_root_pattern("Makefile", dir)
+    local root = lib.files.match_root_pattern("Makefile", dir)
+    log.info("root dir", root)
+    return root
 end
 
 ---@async
+---@param name string Name of directory
+---@return boolean
 function adapter.filter_dir(name, _, _)
     local deny = { '.libs', 'src', '.res', '.git' }
     for _, dir in ipairs(deny) do
         if dir == name then
+            log.info("denied dir", name)
             return false
         end
+        log.info("allowed dir", name)
         return true
     end
 end
 
 ---@async
+---@param file_path string
+---@return boolean
 function adapter.is_test_file(file_path)
-    return file_path:match("Test.kt$")
+    local is_test_file = file_path:match("Test.kt$")
+    log.info("file:", file_path, "is test:", is_test_file)
+    return is_test_file
 end
 
 ---@async
+---@param file_path string Absolute file path
+---@return neotest.Tree | nil
 function adapter.discover_positions(file_path)
     local pos_query = pos.query
+    log.info("query", pos_query)
     return lib.treesitter.parse_positions(file_path, pos_query, {
         require_namespaces = false,
         nested_tests = false,
@@ -40,14 +54,21 @@ function adapter.discover_positions(file_path)
 end
 
 ---@async
+---@param _args neotest.RunArgs
+---@return nil | neotest.RunSpec | neotest.RunSpec[]
 function adapter.build_spec(_args)
+    log.info("build spec", _args)
     return spec.build(_args)
 end
 
 ---@async
+---@param _spec neotest.RunSpec
+---@param _result neotest.StrategyResult
+---@param _tree neotest.Tree
+---@return table<string, neotest.Result>
 function adapter.results(_spec, _res, _tree)
+    log.info("results", _spec, _res, _tree)
     return output.result(_spec, _res, _tree)
 end
 
 return adapter
-
